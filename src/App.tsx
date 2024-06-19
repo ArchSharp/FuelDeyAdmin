a0e8b5fd3d27830b1c744c441884ee1381fa2c36
ayodeji.olalekan <ayodeji.olalekan@konga.com>
2024-06-19 18:47:55 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="">Hello world!</h1>
+      <h2>Testing 2</h2>
+      <h3>Testing 3</h3>
+    </div>
+  );
+}
+
+export default App;
