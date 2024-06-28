e98bf20f7ee7ddce354ec833628e6285a195e0b1
ayodeji.olalekan <ayodeji.olalekan@konga.com>
2024-06-28 16:52:48 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import "./App.css";
+// import "./App.css";
 import { useAppDispatch, useAppSelector } from "./Store/store";
 import { setupAxiosInterceptors } from "./Features/utils";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
