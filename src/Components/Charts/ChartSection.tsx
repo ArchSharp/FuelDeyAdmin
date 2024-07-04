97811f53e5987e7183971a0080b6f1c2b9aa6e56
ayodeji.olalekan <ayodeji.olalekan@konga.com>
2024-07-04 23:18:34 +0100

    update

--- src/Components/Charts/ChartSection.tsx
+++ src/Components/Charts/ChartSection.tsx
@@ -11,7 +11,7 @@ export const ChartSection = ({ title, css }: ChartSectionProps) => {
       className={`${css} flex items-center cursor-pointer rounded-md px-[8px] bg-gradient-to-r from-blue-500 to-green-500`}
     >
       <img src={fuelDrop} alt="fuel-drop" className="w-[15px]" />
-      <div className="font-bold font-poppins text-white ml-2 mr-4 text-sm tracking-wider text-center">
+      <div className="font-bold font-poppins text-white ml-2 mr-4 text-sm tracking-wider text-center text-nowrap">
         {title}
       </div>
     </div>
