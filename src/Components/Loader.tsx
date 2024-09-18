89d8e405033468943b84524d493f4da8a32bedcc
ayodeji.olalekan <ayodeji.olalekan@konga.com>
2024-09-18 22:30:35 +0100

    fixed the loader,is now showing

--- src/Components/Loader.tsx
+++ src/Components/Loader.tsx
@@ -1,6 +1,14 @@
+import { useLocation } from "react-router-dom";
+
 export const Loader = () => {
+  const pathName = useLocation().pathname;
+
   return (
-    <div className="fixed z-[5] top-[43vh] left-[45vw] w-[60px] h-[60px]">
+    <div
+      className={`fixed z-[5] top-[43vh] ${
+        pathName === "/" ? "left-[45vw] md:eft-[60vw]" : "left-[45vw]"
+      } w-[60px] h-[60px]`}
+    >
       <span className="loader"></span>
     </div>
   );
